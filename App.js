dbb2ce7067821c1a63afdf22af4c611a619a8ac8
vivek773 <vk5636040@gmail.com>
2022-04-22 19:24:01 +0530

    final

---
+++ App.js
@@ -0,0 +1,22 @@
+
+let sum1 = 0;
+const increment1 =  () => {
+    sum1 = sum1 + 1;
+    document.getElementById("incrementText1").innerText = sum1;
+}
+let sum2 = 0
+const increment2 =  () => {
+    sum2 = sum2 + 1;
+    document.getElementById("incrementText2").innerText = sum2;
+}
+let sum3 = 0
+const increment3 =  () => {
+    sum3 = sum3 + 1;
+    document.getElementById("incrementText3").innerText = sum3;
+}  
+let sum4 = 0
+const increment4 =  () => {
+    sum4 = sum4 + 1;
+    document.getElementById("incrementText4").innerText = sum4;
+} 
+
